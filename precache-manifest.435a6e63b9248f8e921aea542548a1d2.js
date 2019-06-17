d5129fafa430938aadcde237e24a0bd8a7f5fbf0
Pipas <19pipas@gmail.com>
2019-06-17 16:08:41 +0100

    Updates

---
+++ precache-manifest.435a6e63b9248f8e921aea542548a1d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c18c20397675e424f1f950e9270a125",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa597c88d25ee7ebb3ab",
+    "url": "/static/css/main.8fc25c7e.chunk.css"
+  },
+  {
+    "revision": "4dd7e05fe36e3aa2e4d3",
+    "url": "/static/js/2.d0b992c5.chunk.js"
+  },
+  {
+    "revision": "aa597c88d25ee7ebb3ab",
+    "url": "/static/js/main.1bd7a092.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6c47c74e6413bd809129bfcb151c98fd",
+    "url": "/static/media/PxPlus_IBM_MDA.6c47c74e.woff2"
+  },
+  {
+    "revision": "c29e747edde66dbd297e554d70bfb4fc",
+    "url": "/static/media/PxPlus_IBM_MDA.c29e747e.woff"
+  }
+]);
